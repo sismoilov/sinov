c41753363a3ac5a8d86f0a757a6a0af8b6f979cf
Sardor <sardorismoilov2005@gmail.com>
2025-02-26 00:51:44 +0900

    feat mongoDB ulanish logic

---
+++ app.js
@@ -0,0 +1,45 @@
+console.log("serverni ishga tushirish");
+const express = require("express");
+const app = express();
+// const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
+// mongo db 
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// app.get("/", (req, res) => {
+//   res.end("Hello World!");
+// });
+app.post("/create-item",  (req, res) => {
+  console.log(req.body);
+  res.json({test: "successfully"});
+});
+
+app.get("/", function (req, res) {
+  res.render("reja");
+});
+app.get("/author", (req, res) =>{
+  res.render("author", {user: user});
+} );
+
+// const server = http.createServer(app);
+// let PORT = 3000;
+// server.listen(PORT, () => {
+//   console.log(`Server is running successfully on http://localhost:${PORT}, http://localhost:${PORT}/author`);
+// } );
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,42 +1,23 @@
-console.log("serverni ishga tushirish");
-const express = require("express");
-const app = express();
 const http = require("http");
-const fs = require("fs");
+const mongodb = require("mongodb");
+let db;
+const connectionString = "mongodb+srv://williamismoilov:RO2emViObmvHM2Ok@cluster0.qvw6y.mongodb.net/Reja?retryWrites=true&w=majority&appName=Cluster0&authSource=admin&ssl=true";
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
+mongodb.connect(connectionString, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true }, 
+    (err, client) => {
   if (err) {
-    console.log(err);
+    console.log("mongodb da xato bor:",err);
   } else {
-    user = JSON.parse(data);
-  }
-});
-
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-// app.get("/", (req, res) => {
-//   res.end("Hello World!");
-// });
-app.post("/create-item",  (req, res) => {
-  console.log(req.body);
-  res.json({test: "successfully"});
-});
-
-app.get("/", function (req, res) {
-  res.render("reja");
-});
-app.get("/author", (req, res) =>{
-  res.render("author", {user: user});
-} );
-
-const server = http.createServer(app);
+    console.log("mongodbga ulanish hosil qilindi");
+    module.exports = client;
+    const app = require("./app");
+    const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, () => {
   console.log(`Server is running successfully on http://localhost:${PORT}, http://localhost:${PORT}/author`);
 } );
+
+  }
+});
