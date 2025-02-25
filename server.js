6de64d926d103318020a97aaf18ea50d2171365b
Sardor <sardorismoilov2005@gmail.com>
2025-02-26 00:15:12 +0900

    fix: refactoring

--- server.js
+++ server.js
@@ -1,46 +1,42 @@
 console.log("serverni ishga tushirish");
-
 const express = require("express");
-const res = require("express/lib/response");
 const app = express();
 const http = require("http");
 const fs = require("fs");
 
 let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-fs.readFile("database/user.json", "utf-8", (err, data) => {
-    if (err) {
-      console.log( "ERROR:", err);
-    } else {
-      user = JSON.parse(data);
-    }
-  })
+app.set("views", "views");
+app.set("view engine", "ejs");
 
+// app.get("/", (req, res) => {
+//   res.end("Hello World!");
+// });
+app.post("/create-item",  (req, res) => {
+  console.log(req.body);
+  res.json({test: "successfully"});
+});
 
- app.use(express.static("public"));
- app.use(express.json());
- app.use(express.urlencoded({extended: true}));
+app.get("/", function (req, res) {
+  res.render("reja");
+});
+app.get("/author", (req, res) =>{
+  res.render("author", {user: user});
+} );
 
- app.set("views", "views");
- app.set("view engine", "ejs");
-
- app.post("/create-item", (req, res) => {
-   console.log(req.body);
-   res.json({ test: "success"});
- });
-
- app.get("/author", (req, res) => {  
-   res.render("author", {user:user});
-  });
-
-
- app.get("/", function(req, res) {
-   res.render("reja");
- });
-
- const server = http.createServer(app);
- let port = 3000;
- server.listen(port, function() {
-   console.log(`Server is running on port:  ${port}, http://localhost:${port}`);
- });     
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running successfully on http://localhost:${PORT}, http://localhost:${PORT}/author`);
+} );
