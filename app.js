5b2f5bdae6df3ca482aa525f925edeaa79c964b7
Sardor <sardorismoilov2005@gmail.com>
2025-02-27 20:24:29 +0900

    feat mongoDB ulanish logic

--- app.js
+++ app.js
@@ -1,67 +1,69 @@
-console.log("serverni ishga tushirish");
+console.log("web Serverni boshlash");
+
 const express = require("express");
 const app = express();
-// const http = require("http");
+const http = require("http");
 const fs = require("fs");
 
-// let user;
-// fs.readFile("database/user.json", "utf8", (err, data) => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     user = JSON.parse(data);
-//   }
-// });
-// mongo db 
+let users = [];
+fs.readFile("database/user.json", "utf-8", (err, data) => {
+    if (err) {
+        console.log("ERROR: ", err);
+    } else {
+        users = JSON.parse(data);
+    }
+})
+
+
+// MongoDB chaqirish
 const db = require("./server").db();
+
+
+
+// 1 kirish code 
 
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// 2: Session  code 
+// 3 views code 
 app.set("views", "views");
 app.set("view engine", "ejs");
 
-// app.get("/", (req, res) => {
-//   res.end("Hello World!");
-// });
-app.post("/create-item",  (req, res) => {
-  console.log(req.body);
-  const new_reja = req.body.reja;
-  db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
-    if (err) {
-      console.log(err);
-      res.end("smth went wrong");
-    } else {
-     res.end("successfully added");
-    }
-  } );
-  // res.end("successfully");
-  // res.json({test: "successfully"});
+// 4 routing code
+app.post("/create-item", (req, res) => {
+
+    console.log("user entered /create-item");
+    const new_reja = req.body.reja;
+    db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
+        if (err) {
+            console.log("ERROR: ", err);
+            res.end("something went wrong");
+        } else {
+            res.end("succes");
+        }
+    });
+   // res.json({test:"succes"});
 });
 
-app.get("/", function ( req , res)  {
-  db.collection("plans")
-    .find()
-    .toArray((err, data) => {
-    if (err) {
-      console.log(err);
-      res.end("smth went wrong");
-    } else {
-      console.log(data);
-      res.render("reja", {items: data});
-    }
-  });
-  res.render("reja");
-});
-// app.get("/author", (req, res) =>{
-//   res.render("author", {user: user});
-// } );
 
-// const server = http.createServer(app);
-// let PORT = 3000;
-// server.listen(PORT, () => {
-//   console.log(`Server is running successfully on http://localhost:${PORT}, http://localhost:${PORT}/author`);
-// } );
+
+app.get("/author", (req, res) => {
+    res.render("author", { user:users });
+});
+
+
+app.get("/", function (req, res) {
+    console.log("user entered/");
+    db.collection("plans").find().toArray((err, data) => {
+        if (err) {
+            console.log("ERROR: ", err);
+            res.end("something went wrong");
+        } else {
+            res.render("reja",{items: data });
+        }
+    });
+});
 
 module.exports = app;
